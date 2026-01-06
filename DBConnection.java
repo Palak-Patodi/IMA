3509b9551e99b009f6e62a03ba9fd2b19140ba1d
Palak-Patodi <palakpatodi06@gmail.com>
2026-01-06 15:18:54 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
     private static final String URL =
         "jdbc:mysql://127.0.0.1:3306/inventory_system";
     private static final String USER = "root";
-    private static final String PASSWORD = "pal@0908"; // your mysql password
+    private static final String PASSWORD = ".."; // your mysql password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
