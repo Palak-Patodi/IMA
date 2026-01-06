3d2eaba1d2c9fe66c667c6c3a63349da8941386c
vanshree1635-van <vanshree1635@gmail.com>
2026-01-06 16:23:07 +0530

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -4,16 +4,25 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import ui.LoginUI;
 import ui.DashboardUI;
+import ui.AddInventoryUI;
+import ui.IssueUI;
+import ui.ReturnUI;
+import ui.DeleteInventoryUI;
 
 public class Inventory extends Application {
 
     private Stage stage;
 
+    /**
+     *
+     * @param stage
+     */
     @Override
     public void start(Stage stage) {
         this.stage = stage;
         stage.setTitle("Inventory Management Application");
         stage.setResizable(true);   // enables maximize
+        showDashboard();
         showLogin();
         stage.show();
     }
@@ -33,4 +42,30 @@ public class Inventory extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void showAddInventory() 
+    {
+        AddInventoryUI ui = new AddInventoryUI(this);
+        stage.setScene(ui.getScene());
+    }
+    
+    public void showIssue() 
+    {
+        IssueUI issueUI = new IssueUI(this);
+        stage.setScene(issueUI.getScene());
+    }
+    
+    public void showReturn() 
+    {
+        ReturnUI returnUI = new ReturnUI(this);
+        stage.setScene(returnUI.getScene());
+    }
+
+    public void showDeleteInventory() 
+    {
+        DeleteInventoryUI ui = new DeleteInventoryUI(this);
+        stage.setScene(ui.getScene());
+    }
+
+
 }
